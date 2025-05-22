3dba29cefd61065ce38d3433b0432430f4f2eb97
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2025-05-22 22:34:29 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,26 @@ const App = () => {
       <Router>
         <nav className="navbar navbar-expand-lg w-25 mx-auto mt-5 rounded-pill">
           <div className="container-fluid">
-
-              <ul className="navbar-nav mx-auto">
-                <li>
-                  <Link 
-                    to="/" 
-                    className="text-decoration-none rounded-end-0 btn btn-outline-primary"
-                  >
-                    Local Storage
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    to="/SessionStorage"
-                    className="text-decoration-none rounded-start-0 btn btn-outline-danger"
-                  >
-                    Session Storage
-                  </Link>
-                </li>
-              </ul>
+            <ul className="navbar-nav mx-auto">
+              <li>
+                <Link 
+                  to="/" 
+                  className="text-decoration-none rounded-end-0 btn btn-outline-primary"
+                >
+                  Local Storage
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/SessionStorage"
+                  className="text-decoration-none rounded-start-0 btn btn-outline-danger"
+                >
+                  Session Storage
+                </Link>
+              </li>
+            </ul>
           </div>
         </nav>
-
-        
 
         <Routes>
           <Route path="/" element={<LocalStorage />} />
